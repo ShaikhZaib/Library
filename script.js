5dc8c11ab340787a46ab30f3ed321f6fcede1448
Shaikh Zaib <shaikhzaib04@gmail.com>
2026-07-09 01:03:17 +0530

    Add the js code to add book in library

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
-alert("Hello Zaib here");
+const myLibrary = [];
+
+function Book(title, author, numOfPages, readStatus) {
+  this.id = crypto.randomUUID();
+  this.title = title;
+  this.author = author;
+  this.numOfPages = numOfPages;
+  this.readStatus = readStatus;
+}
+
+function addBookToLibrary(title, author, numOfPages, readStatus) {
+  const book = new Book(title, author, numOfPages, readStatus);
+
+  myLibrary.push(book);
+}
+
+addBookToLibrary("Count of monte carlo", "Alexandar dumas", "600", false);
+
+addBookToLibrary("Harry Potter", "J. K. Rowling", "400", true);
+
+addBookToLibrary("Atomic Habits", "James Clear", "200", true);
+
+// const libraryContainer = document.querySelector(".library-container");
+
+// function displayBooks() {
+//   libraryContainer.innerHTML = "";
+
+//   myLibrary.forEach((book) => {
+//     const bookCard = document.createElement("div");
+//     const h2 = document.createElement("h2");
+
+//     bookCard.classList.add("book-card");
+
+//     h2.innerText = book.title;
+//     bookCard.append(h2);
+
+//     libraryContainer.append(bookCard);
+//   });
+// }
+
+// displayBooks();
